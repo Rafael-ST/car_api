d0c8fd9813195df0eaa9cb04eb37aba12b19239e
Rafael Santos <rafaelsantost87@gmail.com>
2026-05-02 17:03:52 -0300

    migrations com alembic

---
+++ car_api/models/users.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from car_api.models.base import Base
+from datetime import datetime
+from sqlalchemy import func
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    updated_at: Mapped[datetime] = mapped_column(
+        onupdate=func.now(), server_default=func.now(),
+    )
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
